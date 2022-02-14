6c1008c9d578bfded143971bcce1987ec1d22ff7
inkyum <dlsrua0330@naver.com>
2022-02-14 12:35:27 +0900

    테이블 생성 및 데이터 추가부분 수정

--- ebookdb.sql
+++ ebookdb.sql
@@ -18,13 +18,13 @@ DROP TABLE ebook CASCADE CONSTRAINTS;
 
 CREATE TABLE ebook(
     bseq    NUMBER(5)   NOT NULL PRIMARY KEY,
-    title   VARCHAR2(20),
+    title   VARCHAR2(100),
     category CHAR(1),
+    price_rent   NUMBER(7),
     price   NUMBER(7),
     content VARCHAR2(1000) DEFAULT NULL,
     image   VARCHAR2(50),
     useyn   CHAR(1) DEFAULT 'y',
-    bestyn  CHAR(1) DEFAULT 'n',
     likeyn  CHAR(1) DEFAULT 'n',
     regdate DATE DEFAULT SYSDATE
 );
@@ -39,7 +39,7 @@ DROP TABLE manager CASCADE CONSTRAINTS;
 CREATE TABLE manager(
     id  VARCHAR2(20) NOT NULL PRIMARY KEY,
     pwd VARCHAR2(20),
-    name    VARCHAR2(20),
+    name    VARCHAR2(40),
     phone   VARCHAR2(20)
 );
 
@@ -61,6 +61,22 @@ DROP SEQUENCE cart_seq;
 CREATE SEQUENCE cart_seq START WITH 1 INCREMENT BY 1;
 
 
+--리뷰 테이블 생성
+DROP TABLE review CASCADE CONSTRAINTS;
+
+CREATE TABLE review(
+    rseq    NUMBER(10) NOT NULL PRIMARY KEY,
+    id      VARCHAR2(20),
+    bseq   NUMBER(5),
+    rating  NUMBER(2,1),
+    FOREIGN KEY (id) REFERENCES member(id),
+    FOREIGN KEY (bseq) REFERENCES ebook(bseq)
+);
+
+DROP SEQUENCE review_seq;
+CREATE SEQUENCE review_seq START WITH 1 INCREMENT BY 1;
+
+
 --주문 테이블 생성
 DROP TABLE orders CASCADE CONSTRAINTS;
 
@@ -80,9 +96,9 @@ DROP TABLE order_detail;
 
 CREATE TABLE order_detail(
     odseq   NUMBER(10) NOT NULL PRIMARY KEY,
-    oseq    NUMBER(10)  ,
-    bseq    NUMBER(10),
-    result  DEFAULT 'n',
+    oseq    NUMBER(10),
+    bseq    NUMBER(5),
+    result  CHAR(1)  DEFAULT 'n',
     FOREIGN KEY (oseq) REFERENCES orders(oseq),
     FOREIGN KEY (bseq) REFERENCES ebook(bseq)
 );
@@ -100,7 +116,7 @@ CREATE TABLE qna(
     content VARCHAR2(1000), 
     reply   VARCHAR2(1000), 
     id      VARCHAR2(20), 
-    rep     CHAR(1) DEFAULT "n",
+    rep     CHAR(1) DEFAULT 'n',
     indate  DATE DEFAULT SYSDATE,
     FOREIGN KEY (id) REFERENCES member(id)
 );
@@ -116,13 +132,13 @@ CREATE TABLE notice(
     nseq       NUMBER(5)  NOT NULL PRIMARY KEY,
     subject    VARCHAR2(30),
     content    VARCHAR2(1000),
-    id      VARCHAR2(20), 
-    indate     DATE DEFAULT SYSDATE,
-    FOREIGN KEY (id) REFERENCES manager(id)
+    indate     DATE DEFAULT SYSDATE
 );
 
 DROP SEQUENCE notice_seq;
 CREATE SEQUENCE notice_seq START WITH 1 INCREMENT BY 1;
+
+
 
 ---------------샘플데이터-----------------------
 
@@ -131,44 +147,53 @@ insert into manager values('admin', '1234', '관리자', '010-1111-1111');
 
 
 --사용자
-insert into member(id, pwd, name, email, zip_num, address, phone) values
-('one', '1111', '김형주', '06129', '서울특별시 강남구 강남대로96길 16', '017-777-7777');
+insert into member(id, pwd, name, zip_num, address, phone) values
+('one', '1111', '김형주', '06129', '서울특별시 강남구 역삼동 818-3', '017-777-7777');
 insert into member(id, pwd, name, zip_num, address, phone) values
 ('two', '2222', '김민진', '06129', '서울특별시 강남구 역삼동 818-3', '011-123-4567');
 
 --책
-insert into ebook(bseq, title, category, price, content, image) values(
-product_seq.nextval, '책1', '2', '4000', '책 샘플1 입니다.', 'book1.jpg');
-insert into ebook(bseq, title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책2', '2', '5000','책 샘플2 입니다.', 'book2.jpg', 'n');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values( 
-product_seq.nextval, '책3', '1', '10000', '책 샘플3 입니다.', 'book3.jpg', 'n');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책4', '4', '1200', '책 샘플4 입니다.', 'book4.jpg', 'y');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책5', '1', '0', '책 샘플5 입니다.', 'book5.jpg', 'n');
-insert into ebook(bseq,  title, category, price, content, image) values(
-product_seq.nextval, '책6', '6', '0', '책 샘플6 입니다.', 'book6.jpg');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책7', '3', '0', '책 샘플7 입니다.', 'book7.jpg', 'y');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책8', '3', '6000', '책 샘플8 입니다.', 'book8.jpg', 'y');
-insert into ebook(bseq,  title, category, price, content, image) values(
-product_seq.nextval,  '책9', '4', '0', '책 샘플9 입니다.', 'book9.jpg');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책10', '6', '3000', '책 샘플10 입니다.', 'book10.jpg','n');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책11', '5', '11000', '책 샘플11 입니다.', 'book11.jpg','n');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책12', '4', '12000', '책 샘플12 입니다.', 'book12.jpg','n');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책13', '3', '11000', '책 샘플13 입니다.', 'book13.jpg','y');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책14', '2', '13000', '책 샘플14 입니다.', 'book14.jpg','n');
-insert into ebook(bseq,  title, category, price, content, image, bestyn) values(
-product_seq.nextval, '책15', '1', '4000', '책 샘플15 입니다.', 'book15.jpg','y');
+insert into ebook(bseq, title, category, price_rent, price, content, image) values(
+ebook_seq.nextval, '불편한 편의점', '1','2000', '12600', '편의점을 무대로 힘겨운 시대를 살아가는 우리 이웃들의 삶의 속내와 희로애락을 따뜻하고 유머러스하게 담아낸 작품', 'book1.jpg');
+insert into ebook(bseq, title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '센 강의 이름 모를 여인', '1','2500', '14400','고대 그리스의 디오니소스 신화와 센 강을 배경으로 전해 내려오는 ‘데스마스크’ 이야기를 결합시킨 소재로 매우 독특하고 매혹적인 스릴러', 'book2.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values( 
+ebook_seq.nextval, '밝은 밤', '1', '0', '0', '증조모에게서 시작되어 ‘나’에게로 이어지는 이야기와 ‘나’에게서 출발해 증조모로 향하며 쓰이는 이야기가 서로를 넘나들며 서서히 그 간격을 메워가는 이야기', 'book3.jpg');
 
-UPDATE pro
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '세븐 테크', '2','2500', '16200', '가지 테크 혁명들이 어떻게 연결되어 우리의 삶을 바꿀지 생생하게 그릴 수 있게 해준다', 'book4.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '부자 아빠 가난한 아빠', '2','2500', '14220', '유년 시절 겪은 두 아버지를 통해 가난한 사람과 부자의 사고방식을 비교', 'book5.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '돈, 뜨겁게 사랑하고 차갑게 다루어라', '2','0', '0', '투자자 코스톨라니가 넘치는 기지와 유머로 돈의 매력을 탐지하며 증권 거래와 투자심리에 중요한 변수인 투자의 근본적인 비밀과 기술을 설명', 'book6.jpg');
+
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '아직 오지 않은 날들을 위하여', '3','2500', '14400', '‘나이가 들었으면 포기하라’는 건 이제 옛말이다.', 'book7.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '윤석열 X파일', '3','2500', '14400', '20대 대통령에 출마한 윤석열 후보자를 집중적으로 검증하는 책', 'book8.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval,  '벌거벗은 세계사: 사건편', '3','0', '0', 'tvN 〈벌거벗은 세계사〉가 들려준 프레임 밖의 역사적 사건들', 'book9.jpg');
+
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '웰씽킹(Wealthinking)', '4','2500', '14400', '가난했던 소녀공 시절부터 글로벌 기업 회장이 되기까지, 최상위 부자 켈리 최가 말하는 진정한 부에 대한 모든 것', 'book10.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '잘 살아라 그게 최고의 복수다', '4','2500', '13500', '고민들을 갖고 힘들게 살아가는 사람들을 위해, 그 고민들을 해결하기 위한 방법들을 담은 ‘인생 공략집’이다.', 'book11.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '관계를 망치는 사람들을 위한 심리 처방전', '4','0', '0', '관계 심리학자가 알려주는 상처받은 사람이 친밀한 관계를 맺는 법', 'book12.jpg');
+
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '전직 아이돌의 배우 생활', '5','2000', '11000', '멀리서 보면 희극, 가까이서 보면 비극? 다시 사는 인생은 희극이다.', 'book13.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '만능 차트로 키우는 동물병원', '5','1500', '9000', '회귀 후, 동물의 수명과 특이사항이 보이기 시작했다.', 'book14.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '추방당한 각성자는 행복을 꿈꾼다', '5','0', '0', ' 행복한 적 없던 인생. 쉽지 않은 세상이지만, 살아보려 한다.', 'book15.jpg');
+
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '식사하고 가세요!', '6','1000', '8000', '미각을 잃은 요리사, 가상현실게임인 [리얼]에서 다시 요리하다!', 'book16.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '아기가 생겼어요', '6','1000', '8500', '윤리적 교육적으로 완벽한 선생님을 꿈꾸던 내가 하룻 밤의 일탈로 임신?!', 'book17.jpg');
+insert into ebook(bseq,  title, category, price_rent,  price, content, image) values(
+ebook_seq.nextval, '짙은', '6','0', '0', '칼잡이 지운과 목적 없이 살아가는 수한의 짙은 사랑이야기', 'book18.jpg');
 
 -- 장바구니
 insert into cart(cseq, id, bseq) values(cart_seq.nextval, 'one', 1);
@@ -203,12 +228,11 @@ insert into qna (qseq, subject, content, id)
 values(qna_seq.nextval, '테스트2', '질문내용2', 'one');
 
 -- notice
-insert into qna (nseq, subject, content, id)
-values(notice_seq.nextval, '공지1', '공지사항 샘플1', 'admin');
-update qna SET reply='답변내용', rep='y';
+insert into notice (nseq, subject, content)
+values(notice_seq.nextval, '공지1', '공지사항 샘플1');
 
-insert into qna (nseq, subject, content, id)
-values(notice_seq.nextval, '공지2', '공지사항 샘플2', 'admin');
+insert into notice (nseq, subject, content)
+values(notice_seq.nextval, '공지2', '공지사항 샘플2');
 commit;
 
 
@@ -217,77 +241,3 @@ commit;
 --우편번호 테이블 생성 및 데이터 insert
 @zip;
 
-DROP VIEW best_book_view;
---조회시 간단하게 조회하기위해 뷰로 생성
---베스트 상품 조회 뷰
-CREATE OR REPLACE VIEW best_book_view AS
-SELECT bseq, title, price, image
-FROM (SELECT row_number() OVER(ORDER BY regdate)row_num, bseq, title, price, image
-      FROM ebook
-      WHERE bestyn = 'y')
-WHERE row_num <= 4 ;
-
-
-DROP VIEW new_book_view;
---신상품 조회 뷰
-CREATE OR REPLACE VIEW new_book_view AS
-SELECT bseq, title, price, image
-FROM (SELECT row_number() OVER(ORDER BY regdate)row_num, bseq, title, price, image
-      FROM ebook
-      WHERE useyn = 'y')
-WHERE row_num <= 4 ;
-
-
-DROP VIEW cart_view;
---장바구니 보기 View 생성
---필요한 컬럼: 카트일련번호, 회원id, 책번호, 회원명, 책제목, 가격, 장바구니 등록일자, 처리결과
---관련 테이블: cart, member, ebook
-CREATE OR REPLACE VIEW cart_view AS
-SELECT c.cseq, c.id, c.bseq, m.name, e.title,
-        e.price, c.indate, c.result
-FROM cart c, member m, ebook e
-WHERE c.id = m.id
-AND c.bseq = e.bseq
-AND result = 'n';    --주문 미처리
-
-
-DROP VIEW like_view;
---찜하기 보기 View 생성
---필요한 컬럼 : 책번호, 책제목, 가격, 사진
-CREATE OR REPLACE VIEW like_view AS
-SELECT bseq, title, price, image
-FROM (SELECT bseq, title, price, image
-      FROM ebook
-      WHERE likeyn = 'y');
-
-
-DROP VIEW order_view;
---주문 내역 조회 뷰 : order_view
---컬러명: 주문상세번호, 주문번호, 회원ID, 책번호, 책제목, 회원명, 가격, 주문일, 처리결과
-CREATE OR REPLACE VIEW order_view AS
-SELECT d.odseq, o.oseq, o.id, e.bseq, e.title, m.name, e.price, o.indate, d.result
-FROM orders o, order_detail d, ebook e , member m 
-WHERE d.oseq = o.oseq
-AND e.bseq = d.bseq
-AND o.id=m.id;
-
-
---사용자별 주문내역 조회
-SELECT * FROM order_view
-WHERE id='one'
-AND result ='1'
-AND oseq = 2;
-
-
---사용자별 주문번호 목록 조회
-SELECT distinct oseq FROM order_view
-WHERE id='one'
-AND result = '1';
-
-
---페이지별 상품목록 조회
-SELECT bseq, regdate, title, price, useyn, bestyn
-FROM    (SELECT row_number() over(ORDER BY name) rn, bseq, regdate, title, price, useyn, bestyn
-        FROM ebook    
-        WHERE name LIKE '%'||''||'%')
-WHERE rn <= 20 AND rn > 10; 
